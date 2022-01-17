4c873f2416bdf38e39763d5189c154fc612e2a3a
hyunjinkim <a83628670@gmail.com>
2022-01-17 22:20:00 +0900

    Add: human voice cls modeling
    human, human voice, song, life, nature

--- codes/__init__.py
+++ codes/__init__.py
@@ -1,52 +1,29 @@
+import model_predict as mp
+import sys
 import warnings
-import pandas as pd
-import os
-import glob
-import freq_dataset as freq
 warnings.filterwarnings(action='ignore')
-
+mmp = mp.ModelPredict()
 
 if __name__ == '__main__':
-    freq = freq.MakeFreqDataset()
-    # s_path = "../../sounds/"
-    # o_path = list(set(glob.glob(f'{s_path}*/*/*')))  # - set(final_df['path']))
-    # path_ls = [file for file in o_path if file.endswith(".mp3") or file.endswith(".wav")]
-    # name_ls = [name.split('/')[-1][:-4] for name in path_ls]
-    # label_ls = [name.split('/')[-3] for name in path_ls]
-    # info_df = pd.DataFrame()
-    # info_df['path'] = path_ls
-    # info_df['label'] = label_ls
-    # info_df['name'] = name_ls
-    # print(len(info_df))
-    # print(info_df.head)
-    # result_df = freq.make_frequency_df(tracks_df=info_df)
-    # print(result_df)
-    # result_df.to_csv("../files/freq_dataset_1113.csv", encoding='utf-8-sig')
+    try:
+        arg1 = sys.argv[1]
+        try:
+            result_df = mmp.predict_check_result(filename=arg1)
+        except:
+            arg1 = int(arg1)
+            try:
+                arg2 = int(sys.argv[2])
+                result_df = mmp.predict_check_result(start=arg1, end=arg2)
+            except:
+                result_df = mmp.predict_check_result(end=arg1)
 
-    s_path = "../../tracks/1213_trial6/"
-    test_df = pd.read_csv("../files/freq_test_656rows.csv")  # test용 데이터 (90% * 20%) 확보
-    val_df = pd.read_csv("../files/freq_validation_365rows.csv")  # validation용 데이터 (10%) 확보
-    test_df.drop(["Unnamed: 0", "cate1"], axis=1, inplace=True)
-    val_df.drop(["Unnamed: 0", "cate1"], axis=1, inplace=True)
-    info_df = pd.concat([test_df, val_df])
+        print(result_df)
 
-    o_path = list(set(glob.glob(f'{s_path}*/*')))
-    path_ls = [file for file in o_path if file.endswith(".mp3") or file.endswith(".wav")]
-    name_ls = [name.split('/')[-2] for name in path_ls]
-    start_ls = [int(name.split('/')[-1][:-4].split('_')[0]) for name in path_ls]
-    end_ls = [int(name.split('/')[-1][:-4].split('_')[1]) for name in path_ls]
-    label_ls = [info_df[info_df['name'] == name]['cate2'].iloc[0] for name in name_ls]
-    info_df = pd.DataFrame()
-    info_df['path'] = path_ls
-    info_df['name'] = name_ls
-    info_df['start'] = start_ls
-    info_df['end'] = end_ls
-    info_df['label'] = label_ls
-    info_df = info_df.sort_values(by=['name', 'end'])
-    info_df.reset_index(drop=True, inplace=True)
-
-    print(len(info_df))
-    print(info_df.head)
-    result_df = freq.make_frequency_df(tracks_df=info_df)
-    print(result_df)
-    result_df.to_csv("../files/freq_splited_1213_2.csv", encoding='utf-8-sig')
+    except:
+        try:
+            result_df = mmp.predict_check_result()
+            print(result_df.head())
+            print(f"Total number of data: {len(result_df)}")
+        except:
+            print("Please check the type of input data."
+                  "It should be a filename(str) or start, end row number(int).")
--- codes/model_predict.py
+++ codes/model_predict.py
@@ -1,16 +1,15 @@
 import freq_dataset as fd
 import pandas as pd
 import numpy as np
-import random
 import os
 import glob
 import joblib
+import json
+import subprocess
 from pydub import AudioSegment
+import yaml
 from tqdm import tqdm
-import IPython.display
-from sklearn.metrics import accuracy_score, confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
+from datetime import datetime
 import warnings
 warnings.simplefilter("ignore")
 
@@ -18,10 +17,33 @@ warnings.simplefilter("ignore")
 class Preprocessing(fd.MakeFreqDataset):
     def __init__(self):
         super().__init__()
-        self.save_path = "../../tracks/1213_trial6/"
-        self.model_path = "models/"
-        self.filepath = "../files/"
-        self.label_ls = ['bird', 'cat', 'clap', 'dog', 'drum', 'horse', 'lion', 'shout']
+        with open('../config/config.yaml') as f:
+            conf = yaml.load(f, Loader=yaml.FullLoader)
+        self.video_path = conf['video_path']
+        self.wav_from_video_path = conf['wav_from_video_path']
+        self.save_path = conf['save_path']
+        self.file_path = conf['file_path']
+        self.model_path = conf['model_path']
+        self.json_path = conf['json_path']
+        self.label_ls = ["human", "human_voice", "life", "nature", "song"]
+        # local path 에 ffmpeg 가 인식할 수 없는 문자는 치환
+        [os.rename(f, f.replace(' ', '')) for f in glob.glob(f"{self.video_path}*")]
+        [os.rename(f, f.replace('(', '_')) for f in glob.glob(f"{self.video_path}*")]
+        [os.rename(f, f.replace(')', '')) for f in glob.glob(f"{self.video_path}*")]
+        [os.rename(f, f.replace(r'\W', '_')) for f in glob.glob(f"{self.video_path}*")]
+
+    def mp4_to_wav(self):
+        mp4_ls = [file for file in glob.glob(f'{self.video_path}*') if file.endswith(".mp4")]
+        mp4_name_ls = [os.path.basename(p).split('.')[0] for p in mp4_ls].sort()
+        wav_name_ls = [os.path.basename(f).split('.')[0] for f in glob.glob(f'{self.wav_from_video_path}*') if f.endswith(".wav")].sort()
+
+        if mp4_name_ls != wav_name_ls:
+            for mp4_name in mp4_ls:
+                filename = os.path.basename(mp4_name).split('.')[0]
+                command = f"ffmpeg -i {mp4_name} -f wav -acodec libmp3lame -ar 44100 -vn {self.wav_from_video_path}{filename}.wav -y"
+                subprocess.call(command, shell=True)
+        else:
+            pass
 
     def cut_song_by_sec(self, song_path, term=3):
         """
@@ -60,32 +82,43 @@ class Preprocessing(fd.MakeFreqDataset):
         info_df.reset_index(drop=True, inplace=True)
         return info_df
 
-    def edit_frequency_df(self, start=None, end=None):
+    def edit_frequency_df(self, filename=None, start=None, end=None):
         info_df = self.make_info_df()
+        if filename:
+            info_df = info_df[info_df['name'] == filename].copy()
+        else:
+            pass
         freq_df = self.make_frequency_df(info_df, start, end)
         freq_df['start'] = [int(name.split('/')[-1][:-4].split('_')[0]) for name in freq_df['path']]
         freq_df['end'] = [int(name.split('/')[-1][:-4].split('_')[1]) for name in freq_df['path']]
         # freq_df['label'] = [info_df[info_df['name'] == name]['cate2'].iloc[0] for name in freq_df['name']]
         return freq_df
 
-
-class ModelPredict(Preprocessing):
-    def __init__(self):
-        super().__init__()
-
-    def load_model_ls(self, filters):
+    def load_model_ls(self, filters=None):
         """
         :param filters: 불러올 model의 필터 조건을 regex문법의 string으로 여러 개의 조건을 list type 으로 input
                         ex: ['1213_*XGB*' , '1213_*RF*']
         :return: model list
         """
-        model_ls = []
-        for filter in filters:
-            tmp_ls = list(set(glob.glob(f'{self.model_path}{filter}')))
-            model_ls += tmp_ls
+        if filters is None:
+            model_ls = list(set(glob.glob(f'{self.model_path}*')))
+        else:
+            model_ls = []
+            for filter in filters:
+                tmp_ls = list(set(glob.glob(f'{self.model_path}{filter}')))
+                model_ls += tmp_ls
         print(f"총 {len(model_ls)} 개의 모델이 있습니다.")
         print(model_ls)
         return model_ls
+
+
+class ModelPredict(Preprocessing):
+
+    def __init__(self):
+        super().__init__()
+        self.mp4_to_wav()  # mp4 동영상 에서 wav 음원 추출
+        self.info_df = self.make_info_df()
+        self.model_ls = self.load_model_ls()
 
     def model_predict(self, model, target_df):
         md = joblib.load(model)
@@ -94,10 +127,10 @@ class ModelPredict(Preprocessing):
         print(f"이 소리는 {(max(md.predict_proba(tmp_df)[0]) * 100).round(3)}의 확률로 '{md.predict(tmp_df)[0]}' 로 추정됩니다.")
         return md.predict_proba(tmp_df)[0], md.predict(tmp_df)[0]
 
-    def make_predict_df(self, filters, start=None, end=None):
+    def make_predict_df(self, filters=None, filename=None, start=None, end=None):
         print(self.label_ls)
         model_ls = self.load_model_ls(filters)  # 조건에 맞는 모델 리스트 가져오기
-        val_df = self.edit_frequency_df(start, end)  # save_path에 저장되어있는 음원으로 frequency 생성
+        val_df = self.edit_frequency_df(filename, start, end)  # save_path에 저장되어있는 음원으로 frequency 생성
 
         final_result = pd.DataFrame()
         for idx in tqdm(range(len(val_df))):
@@ -112,6 +145,8 @@ class ModelPredict(Preprocessing):
             result_df = pd.DataFrame(data=proba_ls, columns=self.label_ls).round(1)
             result_df['model'] = [m.split("/")[-1][:-4] for m in model_ls]
             result_df['name'] = freq_df['name'][idx]
+            result_df['start'] = freq_df['start'][idx]
+            result_df['end'] = freq_df['end'][idx]
             result_df['y_pred'] = pred_label
             result_df.sort_values(by=f'{self.label_ls[sum(proba_ls).tolist().index(max(sum(proba_ls)))]}',
                                   ascending=False, inplace=True)
@@ -119,13 +154,55 @@ class ModelPredict(Preprocessing):
 
         return final_result
 
+    def save_json(self, target_df, name_ls=None):
+        if len(target_df) == 1:
+            new = []
+            for idx in range(len(self.label_ls)):
+                new.append([f"{self.label_ls[idx]}",
+                            f"{round(target_df.iloc[0][idx]*100, 2)}"])
+            with open(f"{self.json_path}{target_df.index[0][0]}_{target_df.index[0][1]}.json", "w") as json_file:
+                json.dump(new, json_file, indent=True)
+
+        elif len(target_df) > 1:
+            total = []  #쌓기코드
+            for idx, song in enumerate(target_df.index.tolist()):
+                new = []
+                row = target_df.loc[song]
+                for idx in range(len(self.label_ls)):
+                    new.append([f"{self.label_ls[idx]}",
+                                f"{round(row[idx]*100, 2)}"])
+                with open(f"{self.json_path}{target_df.index[idx][0]}_{target_df.index[idx][1]}.json", "w") as json_file:
+                    json.dump(new, json_file, indent=True)
+                total.append(new)  #쌓기코드
+            with open(f"{self.json_path}{target_df.index[0][0]}_{target_df.index[0][1]}_{target_df.index[-1][1]}_"
+                      f"{datetime.today().strftime('%Y%m%d')}.json", "w") as json_file:
+                json.dump(total, json_file, indent=True)
+        else:
+            print("Failed to save json file!")
+
+    def predict_check_result(self, filters=None, filename=None, start=None, end=None):
+        result_df = self.make_predict_df(filters, filename, start, end)
+        result_df = result_df.drop(['end'], axis=1)
+        result_df.set_index('start', drop=True, inplace=True)
+        pct_df = result_df.groupby(['name', 'start']).sum() / len(self.model_ls)
+        for name, item in pct_df.iterrows():
+            print(f"'{name}' 은 {round(max(item), 2) * 100}% 의 확률로"
+                  f" {item.index[item.tolist().index(max(item))]}로 추정됩니다.")
+        name_ls = []
+        self.save_json(pct_df, name_ls)
+        human_pct = (pct_df * 100).iloc[:, :2].sum(axis=1)
+        non_human_pct = (pct_df * 100).iloc[:, 2:].sum(axis=1)
+        pct = pd.DataFrame(human_pct, columns=['Human'])
+        pct['Non-human'] = non_human_pct
+        return result_df, pct
+
 
 class FreqEDA(Preprocessing):
     def __init__(self):
         super().__init__()
 
     def read_csv(self, filename):
-        csvfile = pd.read_csv(f"{self.filepath}{filename}.csv", encoding='utf-8-sig')
+        csvfile = pd.read_csv(f"{self.file_path}{filename}.csv", encoding='utf-8-sig')
         try:
             csvfile.drop(["Unnamed: 0"], axis=1, inplace=True)
         except:
